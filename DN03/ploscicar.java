41722638a18ecb53daafe065846d9475afd84c6d
zakjeg <zak.jeglic@gmail.com>
2025-11-07 17:44:11 +0100

    DN03

--- DN03/ploscicar.java
+++ DN03/ploscicar.java
@@ -4,31 +4,51 @@ public class ploscicar{
 	public static void main(String[] args){
 		Scanner sc = new Scanner(System.in);
 		int h = sc.nextInt();int w=sc.nextInt();int k=sc.nextInt();
-		int ploscina = h*w;
-		int ploscice = 0;
+		long sum=0;
 
-		while(Math.pow(2,k)>h || Math.pow(2,h)>w){
-			k--;
+		int visinaStolpca=0;
+		int sirinaStolpca=0;
+		long steviloPloscic=0;
+		while((w)>0){
+			
+			int n=najvecjiK(h,w,k);
+			int naN=(int)Math.pow(2,n);
+			sirinaStolpca=naN;
+
+			while(visinaStolpca<h){
+
+				steviloPloscic+=((h-visinaStolpca)/naN)*(sirinaStolpca/naN);
+				visinaStolpca+=((h-visinaStolpca)/naN)*naN;
+
+				naN=(int)Math.pow(2,--n);
+
+			}
+			
+			sum+=steviloPloscic*(w/sirinaStolpca);
+			w-=(w/sirinaStolpca)*sirinaStolpca;
+			visinaStolpca=0;
+			steviloPloscic=0;
+
 		}
 
-		while(w>0){
+		System.out.println(sum);
 
-			while(Math.pow(2,k)>h || Math.pow(2,h)>w){
-        	                k--;
-               		}
-
-			int visina=0; int sirina=0;
-			while(visina<=h){
-				visina+=((h/Math.pow(2,k))*Math.pow(2,k));
-				
-				k--
-			}
-			for(int i=k; i>-1;i--){
-				int ploscica=Math.pow(2,i);
-				
-			}
-		}
-
-		System.out.println(h + " " + w + " " + k);
 	}
+
+	public static int najvecjiK(int h, int w, int k){
+
+		int size = (int)Math.pow(2,k);
+		while(size>h || size>w){
+		k--;
+		size=(int)Math.pow(2,k);
+		}
+
+
+		return k;
+	}
+	
+
+	public static int stPloscic(int a){
+		return -1;
+	} 
 }
